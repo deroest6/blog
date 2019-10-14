d379bb8a93ca9c7be935c87ab17ff519bb06f405
Jantsen DeRoest <deroest6@hotmail.com>
2019-10-14 09:19:50 -0600

    modified listening port

--- app.js
+++ app.js
@@ -89,12 +89,13 @@ app.get("/posts/:postId", function(req, res) {
     }
   });
 
-
 });
 
 
-const port = process.env.PORT || 3000;
-
-app.listen(port, function() {
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
