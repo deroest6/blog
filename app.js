d30df9c9741037e3f9bfff47b8eaaf29c46ad967
Jantsen DeRoest <deroest6@hotmail.com>
2019-10-14 13:30:23 -0600

    mon.connect mod - added createIndex

--- app.js
+++ app.js
@@ -13,7 +13,11 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb+srv://admin-jantsen:test123@cluster0-ivwmp.mongodb.net/BlogDB', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect('mongodb+srv://admin-jantsen:test123@cluster0-ivwmp.mongodb.net/BlogDB',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+});
 // mongoose.connect("mongodb://localhost:27017/blogDB", {
 //   useNewUrlParser: true
 // });
