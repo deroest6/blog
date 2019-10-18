587db2e31c29ff889f4c76840fa48357f7589dd2
Jantsen DeRoest <deroest6@hotmail.com>
2019-10-17 20:35:10 -0600

    Revised the listening port config

--- app.js
+++ app.js
@@ -100,6 +100,7 @@ let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
-app.listen(3000, function() {
+
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
